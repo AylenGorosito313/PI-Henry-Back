e698b6edee03003dca9f29cf62a4f83d7d509e75
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2022-12-26 19:19:07 -0300

    a

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB
+  DB_USER, PGPASSWORD, DB_HOST,DB
 } = process.env;
 
 
